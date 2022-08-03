e9799dabcd5ed5dfe9f888772c812d3741fe3468
jacob <jhm0068@auburn.edu>
2022-08-02 21:22:11 -0500

    button

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 from tkinter import *
 
-#setting up the app's basic window settings
-root = Tk()
-root.title("Insidious 1.0")
-root.geometry("1280x720")
+current_page = 0
 
-# Specify Grid
-Grid.rowconfigure(root, 0, weight=1)
-Grid.columnconfigure(root, 0, weight=1)
 
-Grid.rowconfigure(root, 1, weight=1)
+def dashboard_setup():
+    # setting up the app's basic window settings
+    root.title("Insidious 1.0")
+    root.geometry("1280x720")
 
-# Create Buttons
-button_1 = Button(root, text="Button 1")
-button_2 = Button(root, text="Button 2")
+    # Specify Grid
+    # row specifications
+    root.rowconfigure(0, weight=1)
+    root.rowconfigure(1, weight=1)
+    root.rowconfigure(2, weight=1)
+    root.rowconfigure(3, weight=3)
 
-# Set grid
-button_1.grid(row=0, column=0, sticky="NSEW")
-button_2.grid(row=1, column=0, sticky="NSEW")
+    # column specifications
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=8)
 
-root.mainloop()
+    # Create Buttons
+    dashboard_btn = Button(root, text="Dashboard")
+    tasks_btn = Button(root, text="Tasks")
+    settings_btn = Button(root, text="Settings")
+
+    # Set grid
+    dashboard_btn.grid(row=0, column=0, sticky="NSEW")
+    tasks_btn.grid(row=1, column=0, sticky="NSEW")
+    settings_btn.grid(row=2, column=0, sticky="NSEW")
+
+
+# MAIN LOOP
+if __name__ == "__main__":
+    root = Tk()
+    # Sets up the home page GUI
+    home_page_setup()
+
+    root.mainloop()
+
 
