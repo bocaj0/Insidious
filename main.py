1e9902a7d7ab4610db0ab4be3d747382eb2addc8
jacob <jhm0068@auburn.edu>
2022-08-07 00:05:32 -0500

    tab working, individual screens are not deleting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+# Tracks the page of the app GUI
+base_page = 0
+
 
 class MainGui:
     def __init__(self, app, title, geometry):
@@ -53,71 +56,93 @@ class BasePage:
     def __init__(self, parent, window):
         self.parent = parent
         self.window = window
-        self.frame = Frame(window)
-        self.frame.pack()
-
-        # self.header = Label(self.frame, text="")
-        # self.header.grid(row=0, column=1)
+        self.frame = Frame(window, width=400, height=720, bg="red")
+        self.frame.pack(side=LEFT, anchor='nw', fill='both')
 
         # Create Buttons
-        self.dashboard_btn = Button(self.frame, text="Dashboard", command=self.change_dash_page)
-        self.tasks_btn = Button(self.frame, text="Tasks", command=self.change_task_page)
-        self.settings_btn = Button(self.frame, text="Settings", command=self.change_sett_page)
+        self.dashboard_btn = Button(self.frame, text="\tDashboard\t", command=self.change_dash_page)
+        self.tasks_btn = Button(self.frame, text="\t    Tasks\t\t", command=self.change_task_page)
+        self.settings_btn = Button(self.frame, text="\t  Settings\t", command=self.change_sett_page)
 
         # Set grid
-        self.dashboard_btn.grid(row=0, column=0, sticky="NW")
-        self.tasks_btn.grid(row=1, column=0, sticky="NW")
-        self.settings_btn.grid(row=2, column=0, sticky="NW")
+        self.dashboard_btn.grid(row=0, column=0)
+        self.tasks_btn.grid(row=1, column=0)
+        self.settings_btn.grid(row=2, column=0)
 
-        # self.show_page = DashboardPage(self, self.window)
+        self.show_page = DashboardPage(self, self.window)
 
     def change_dash_page(self):
-        self.frame.destroy()
-        self.show_page = DashboardPage(self, self.window)
+        global base_page
+        if base_page != 0:
+            base_page = 0
+            self.show_page = DashboardPage(self, self.window)
 
     def change_task_page(self):
-        self.frame.destroy()
-        self.show_page = DashboardPage(self, self.window)
+        global base_page
+        if base_page != 1:
+            base_page = 1
+            self.show_page = TasksPage(self, self.window)
 
     def change_sett_page(self):
-        self.frame.destroy()
-        self.show_page = DashboardPage(self, self.window)
+        global base_page
+        if base_page != 2:
+            base_page = 2
+            self.show_page = SettingsPage(self, self.window)
 
 
 class DashboardPage:
     def __init__(self, parent, window):
-        self.parent = parent
-        self.window = window
-        self.frame = Frame(window)
+        global base_page
+        self.frame = Frame(window, bg="blue")
         self.frame.pack()
 
         # Welcome button
-        self.welcome_lbl = Label(self.frame, text='IT WORKED!')
+        self.welcome_lbl = Label(self.frame, text='dash!')
         self.welcome_lbl.grid(row=0, column=1)
 
         # Login button
-        self.login = Button(self.frame, text='TEST', command=self.test)
+        self.login = Button(self.frame, text='TEST1')
         self.login.grid(row=3, column=1)
 
-    def test(self):
-        self.frame.destroy()
-        self.parent.parent.change_page(1)
+        if base_page != 0:
+            self.frame.destroy()
+            # self.parent.parent.change_page(0)
 
 
-    # def tasks_clicked(self):
-        # self.parent.change_page(2)
-        # creates tasks page header
-        # self.header.grid_forget()
-        # self.header = Label(self.frame, text="Tasks")
-        # self.header.grid(row=0, column=1)
+class TasksPage:
+    def __init__(self, parent, window):
+        global base_page
+        self.frame = Frame(window, bg="blue")
+        self.frame.pack()
 
-    # def settings_clicked(self):
-        # self.frame.destroy()
-        # self.parent.change_page(3)
-        # creates settings page header
-        # self.header.grid_forget()
-        # self.header = Label(self.frame, text="Settings")
-        # self.header.grid(row=0, column=1)
+        # Welcome button
+        self.welcome_lbl = Label(self.frame, text='TASKS!')
+        self.welcome_lbl.grid(row=0, column=1)
+
+        # Login button
+        self.login = Button(self.frame, text='TEST2')
+        self.login.grid(row=3, column=1)
+
+        if base_page != 1:
+            self.frame.destroy()
+
+
+class SettingsPage:
+    def __init__(self, parent, window):
+        global base_page
+        self.frame = Frame(window, bg="blue")
+        self.frame.pack()
+
+        # Welcome button
+        self.welcome_lbl = Label(self.frame, text='Settings!')
+        self.welcome_lbl.grid(row=0, column=1)
+
+        # Login button
+        self.login = Button(self.frame, text='TEST3')
+        self.login.grid(row=3, column=1)
+
+        if base_page != 2:
+            self.frame.destroy()
 
 
 if __name__ == '__main__':
