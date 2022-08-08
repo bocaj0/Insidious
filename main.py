53d23cf1839f8d2a23fa3de2968fc03423077553
jacob <jhm0068@auburn.edu>
2022-08-07 19:00:07 -0500

    page switching is WORKING!!!!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 # Tracks the page of the app GUI
-base_page = 0
+curr_page = 0
+prev_page = 0
 
 
 class MainGui:
@@ -52,6 +53,27 @@ class LoginPage:
         self.parent.change_page(1)
 
 
+# changes the pages/creates new pages for dashboard/tasks/settings
+def page_change(current, num):
+    global curr_page, prev_page
+    curr_page = num
+    if curr_page != prev_page:
+        current.show_page.frame.destroy()
+
+    if curr_page == 0:
+        if prev_page != 0:
+            current.show_page = DashboardPage(current, current.window)
+    if curr_page == 1:
+        if prev_page != 1:
+            current.show_page = TasksPage(current, current.window)
+    if curr_page == 2:
+        if prev_page != 2:
+            current.show_page = SettingsPage(current, current.window)
+
+    prev_page = num
+
+
+# creates the side labels of dashboard/tasks/settings
 class BasePage:
     def __init__(self, parent, window):
         self.parent = parent
@@ -60,89 +82,65 @@ class BasePage:
         self.frame.pack(side=LEFT, anchor='nw', fill='both')
 
         # Create Buttons
-        self.dashboard_btn = Button(self.frame, text="\tDashboard\t", command=self.change_dash_page)
-        self.tasks_btn = Button(self.frame, text="\t    Tasks\t\t", command=self.change_task_page)
-        self.settings_btn = Button(self.frame, text="\t  Settings\t", command=self.change_sett_page)
+        self.dashboard_btn = Button(self.frame, text="\tDashboard\t", command=lambda: page_change(self, 0))
+        self.tasks_btn = Button(self.frame, text="\t    Tasks\t\t", command=lambda: page_change(self, 1))
+        self.settings_btn = Button(self.frame, text="\t  Settings\t", command=lambda: page_change(self, 2))
+        self.logout_btn = Button(self.frame, text="\t  Logout\t\t", command=self.clicked)
 
         # Set grid
         self.dashboard_btn.grid(row=0, column=0)
         self.tasks_btn.grid(row=1, column=0)
         self.settings_btn.grid(row=2, column=0)
+        self.logout_btn.grid(row=3, column=0)
 
         self.show_page = DashboardPage(self, self.window)
 
-    def change_dash_page(self):
-        global base_page
-        if base_page != 0:
-            base_page = 0
-            self.show_page = DashboardPage(self, self.window)
-
-    def change_task_page(self):
-        global base_page
-        if base_page != 1:
-            base_page = 1
-            self.show_page = TasksPage(self, self.window)
-
-    def change_sett_page(self):
-        global base_page
-        if base_page != 2:
-            base_page = 2
-            self.show_page = SettingsPage(self, self.window)
+    def clicked(self):
+        page_change(self, 10)  # random number that are not equal and do not represent a page
+        self.frame.destroy()
+        self.parent.change_page(0)
 
 
 class DashboardPage:
     def __init__(self, parent, window):
-        global base_page
-        self.frame = Frame(window, bg="blue")
+        self.frame = Frame(window, bg="green")
         self.frame.pack()
 
         # Welcome button
-        self.welcome_lbl = Label(self.frame, text='dash!')
+        self.welcome_lbl = Label(self.frame, text='DASHBOARD PAGE')
         self.welcome_lbl.grid(row=0, column=1)
 
         # Login button
         self.login = Button(self.frame, text='TEST1')
         self.login.grid(row=3, column=1)
 
-        if base_page != 0:
-            self.frame.destroy()
-            # self.parent.parent.change_page(0)
-
 
 class TasksPage:
     def __init__(self, parent, window):
-        global base_page
-        self.frame = Frame(window, bg="blue")
+        self.frame = Frame(window, bg="yellow")
         self.frame.pack()
 
         # Welcome button
-        self.welcome_lbl = Label(self.frame, text='TASKS!')
+        self.welcome_lbl = Label(self.frame, text='TASKS PAGE')
         self.welcome_lbl.grid(row=0, column=1)
 
         # Login button
         self.login = Button(self.frame, text='TEST2')
         self.login.grid(row=3, column=1)
 
-        if base_page != 1:
-            self.frame.destroy()
-
 
 class SettingsPage:
     def __init__(self, parent, window):
-        global base_page
         self.frame = Frame(window, bg="blue")
         self.frame.pack()
 
         # Welcome button
-        self.welcome_lbl = Label(self.frame, text='Settings!')
+        self.welcome_lbl = Label(self.frame, text='SETTINGS PAGE')
         self.welcome_lbl.grid(row=0, column=1)
 
         # Login button
         self.login = Button(self.frame, text='TEST3')
         self.login.grid(row=3, column=1)
-
-        if base_page != 2:
-            self.frame.destroy()
 
 
 if __name__ == '__main__':
